bec0014ee0dc389b8e6af255fbf49c2679bd30c8
José Ferreira <jose.ferreira@mindswap.academy>
2023-03-07 16:01:25 +0000

    Nav bar and logo position

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Logo.css";
+
+export default function Logo() {
+  return (
+    <div>
+      <img
+      className="pokedex-img"
+        src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png"
+        alt="pokedex"
+      ></img>
+    </div>
+  );
+}
