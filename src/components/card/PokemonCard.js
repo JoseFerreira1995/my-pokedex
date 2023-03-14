59ca0352baa3bc8bbda7a4236f79af0b591629e0
José Ferreira <jose.ferreira@mindswap.academy>
2023-03-14 12:47:23 +0000

    Fetched pokeApi/sprites to get pokemon img

--- src/components/card/PokemonCard.js
+++ src/components/card/PokemonCard.js
@@ -2,14 +2,16 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 export default function PokemonCard(props) {
-  const { pokemon } = props;
+  const { pokemon, sprite } = props;
 
   return (
-    <Card border="dark" style={{ width: "18rem" }}>
-      <Card.Img variant="top" />
+    <Card style={{ width: "10rem", height: "10rem" }}>
+      <Card.Img variant="top" src={sprite} alt={pokemon.name} />
       <Card.Body>
         <Card.Title>{pokemon.name}</Card.Title>
-        <Button variant="outline-light" onClick={pokemon.url}>Info</Button>
+        <Button variant="outline-light" onClick={pokemon.url}>
+          Info
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/components/list/PokemonList.js
+++ src/components/list/PokemonList.js
@@ -4,25 +4,38 @@ import { useEffect } from "react";
 import PokemonCard from "../card/PokemonCard";
 
 export default function PokemonList(props) {
-  const [pokemonData, setPokemonData] = useState(null);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonSprites, setPokemonSprites] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon")
       .then((response) => {
-        setPokemonData(response.data);
+        setPokemonData(response.data.results);
+        return response.data.results;
+      })
+      .then((pokemon) => {
+        const spriteRequest = pokemon.map((p) =>
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${p.name}`)
+        );
+        return Promise.all(spriteRequest);
+      })
+      .then((responses) => {
+        const spriteData = responses.map((r) => r.data.sprites.front_default);
+        setPokemonSprites(spriteData);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-  
+
   return (
     <div>
-      {pokemonData &&
-        pokemonData.results.map((pokemon, index) => (
-          <PokemonCard key={index} pokemon={pokemon} />
-        ))}
+      {pokemonData.map((pokemon, index) => (
+        <div>
+          <PokemonCard key={index} pokemon={pokemon} sprite={pokemonSprites[index]} />
+        </div>
+      ))}
     </div>
   );
 }
